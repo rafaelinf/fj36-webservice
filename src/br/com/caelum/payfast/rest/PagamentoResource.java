e2b91438fdc4df75531f0f34bf9c8f70792bc45e
rafaelinf <rafael.nogueira2009@gmail.com>
2016-02-27 16:48:54 -0300

    Foi adicionado o metodo POST no recurso pagamento

--- src/br/com/caelum/payfast/rest/PagamentoResource.java
+++ src/br/com/caelum/payfast/rest/PagamentoResource.java
@@ -1,17 +1,23 @@
 package br.com.caelum.payfast.rest;
 
 import java.math.BigDecimal;
+import java.net.URI;
+import java.net.URISyntaxException;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.ejb.Singleton;
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import br.com.caelum.payfast.modelo.Pagamento;
+import br.com.caelum.payfast.modelo.Transacao;
 
 @Path("/pagamentos")
 @Singleton
@@ -22,7 +28,7 @@ public class PagamentoResource {
 
 	public PagamentoResource() {
 		Pagamento pagamento = new Pagamento();
-		pagamento.setId(idPagamento);
+		pagamento.setId(idPagamento++);
 		pagamento.setValor(BigDecimal.TEN);
 		pagamento.comStatusCriado();
 		repositorio.put(pagamento.getId(), pagamento);
@@ -34,4 +40,24 @@ public class PagamentoResource {
 	public Pagamento buscaPagamento(@PathParam("id") Integer id){
 		return repositorio.get(id);
 	}
+	
+	@POST
+	@Consumes({MediaType.APPLICATION_JSON})
+	public Response criarPagamento(Transacao transacao) throws URISyntaxException{
+		
+		System.out.println(transacao.toString());
+		
+		Pagamento pagamento = new Pagamento();
+		pagamento.setId(idPagamento++);
+		pagamento.comStatusCriado();
+		pagamento.setValor(transacao.getValor());
+		
+		repositorio.put(pagamento.getId(), pagamento);
+		
+		System.out.println("PAGAMENTO CRIADO " + pagamento);
+		
+		return Response.created(new URI("/pagamentos/" + pagamento.getId())).entity(pagamento).type(MediaType.APPLICATION_JSON_TYPE).build();
+		
+	}
+	
 }
